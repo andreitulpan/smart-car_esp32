ac26a4b510e5add219cc12c8b8ffd8231fb4e82a
Andrei Tulpan <andrei.tulpan@previsesystems.com>
2025-05-21 17:49:16 +0300

    LOG service

---
+++ src/LOG/LogHandler.cpp
@@ -0,0 +1,39 @@
+#include "LogHandler.hpp"
+
+std::queue<LogHandler::LogEntry> LogHandler::logQueue;
+
+unsigned long LogHandler::getTime() {
+    time_t now;
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo) || timeinfo.tm_year < (2020 - 1900)) {
+        // Time not set or invalid
+        return 0;
+    }
+    time(&now);
+    return now;
+}
+
+void LogHandler::writeMessage(LogHandler::DebugType type, const String& message, bool sendToFirebase) {
+    String typeStr;
+    switch (type) {
+        case DebugType::INFO: typeStr = "INFO"; break;
+        case DebugType::WARNING: typeStr = "WARNING"; break;
+        case DebugType::ERROR: typeStr = "ERROR"; break;
+        case DebugType::BLE: typeStr = "BLE"; break;
+        case DebugType::CAN: typeStr = "CAN"; break;
+        default: typeStr = "INFO"; break;
+    }
+    if (sendToFirebase) {
+        logQueue.push({typeStr, String(getTime()), message});
+    }
+    Serial.println("[" + typeStr + "] " + message);
+}
+
+std::vector<LogHandler::LogEntry> LogHandler::getAndClearLogs() {
+    std::vector<LogEntry> logs;
+    while (!logQueue.empty()) {
+        logs.push_back(logQueue.front());
+        logQueue.pop();
+    }
+    return logs;
+}
---
+++ src/LOG/LogHandler.hpp
@@ -0,0 +1,31 @@
+#ifndef DEBUG_HANDLER_HPP
+#define DEBUG_HANDLER_HPP
+
+#include <Arduino.h>
+#include <queue>
+#include <vector>
+
+class LogHandler {
+public:
+    enum class DebugType {
+        INFO,
+        BLE,
+        WARNING,
+        CAN,
+        ERROR
+    };
+
+    struct LogEntry {
+        String path;
+        String timestamp;
+        String message;
+    };
+
+    static unsigned long getTime();
+    static void writeMessage(LogHandler::DebugType type, const String& message, bool sendToFirebase = true);
+    static void sendLogMessage(const String& path, const String& message);
+    static std::vector<LogHandler::LogEntry> getAndClearLogs();
+    static std::queue<LogHandler::LogEntry> logQueue;
+};
+
+#endif // DEBUG_HANDLER_HPP
